c809022cb7e8858352623abc300177be92d57334
猪野 湧斗 <ice.softicon30th@gmail.com>
2016-03-13 05:21:38 +0900

    なんでも する いい ない

--- User.js
+++ User.js
@@ -1,7 +1,8 @@
 var Twit = require('twit');
 
 var regExpNandemoii = /(なん|何)でも(良|い)い/g;
-var regExpNandemo = /(なん|何)でも((?!かんでも)|(?!ない))/g;
+var regExpNandemosuru = /(なん|何)でも((する)|(します)|(やる)|(やります))/g;
+var regExpNandemonai = /(なん|何)でも((ない)|(ありません)|(ございませ)/g;
 
 function User(consumerKey, consumerSecret, accessToken, accessTokenSecret) {
 	var client = new Twit({
@@ -14,18 +15,24 @@ function User(consumerKey, consumerSecret, accessToken, accessTokenSecret) {
 	stream.on('tweet', function(tweet) {
 		if (/\?|？/g.test(tweet.text)) return;
         if (/(い|言)ったよね/g.test(tweet.text)) return;
-		var messagePrefix = '@' + tweet.user.screen_name + ' ';
+        
+        var match = false;
+        var message = '@' + tweet.user.screen_name + ' ';
+        
 		if (regExpNandemoii.test(tweet.text)) {
-			client.post('statuses/update', {
-				status: messagePrefix + 'ん？今なんでもいいって言ったよね',
-                in_reply_to_status_id: tweet.id_str
-			});
-		} else if (regExpNandemo.test(tweet.text)) {
-			client.post('statuses/update', {
-				status: messagePrefix + 'ん？今なんでもするって言ったよね',
-                in_reply_to_status_id: tweet.id_str
-			});
+            message += 'ん？今なんでもいいって言ったよね'; 
+		} else if (regExpNandemosuru.test(tweet.text)) {
+            message += 'ん？今なんでもするって言ったよね';
+		} else if (regExpNandemonai.test(tweet.text)) {
+            message += 'ん？今なんでもないって言ったよね';
 		}
+        
+        if (match) {
+			client.post('statuses/update', {
+				status: message,
+                in_reply_to_status_id: tweet.id_str
+			});
+        }
 	});
 }
 
