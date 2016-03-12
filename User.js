713f2e9d19f0d01bc53ac31a4bb8e3d28fe3ac04
猪野 湧斗 <ice.softicon30th@gmail.com>
2016-03-13 04:12:07 +0900

    sources

---
+++ User.js
@@ -0,0 +1,35 @@
+var Twit = require('twit');
+var CONSUMER_KEY = null,
+	CONSUMER_SECRET = null;
+
+UserStream.prototype.regExpNandemoii = /(なん|何)でも(良|い)い/g;
+UserStream.prototype.regExpNandemo = /(なん|何)でも((?!かんでも)|(?!ない))/g;
+
+function User(accessToken, accessTokenSecret) {
+	var client = new Twit({
+		consumer_key: CONSUMER_KEY,
+		consumer_secret: CONSUMER_SECRET,
+		access_token: accessToken,
+		access_token_secret:accessTokenSecret
+	});
+	var stream = client.stream;
+	stream.on('tweet', function(tweet) {
+		if (/\?/g.test(tweet.text)) return;
+		var messagePrefix = '@' + tweet.user.screen_name + ' ';
+		if (this.regExpNandemoii.test(tweet.text)) {
+			client.post('statuses/update', {
+				status: messagePrefix + 'ん？今なんでもいいって言ったよね'
+			});
+		} else if (this.regExpNandemo.test(tweet.text)) {
+			client.post('statuses/update', {
+				status: messagePrefix + 'ん？今なんでもするって言ったよね';
+			});
+		}
+	});
+}
+
+module.exports = function (consumerKey, consumerSecret) {
+	CONSUMER_KEY = consumerKey;
+	CONSUMER_SECRET = consumerSecret;
+	return User;
+};
---
+++ index.js
@@ -0,0 +1,81 @@
+var ConsumerKey = require('./ConsumerKey.json');
+var User = require('./User.js')(ConsumerKey.consumer_key, ConsumerKey.consumer_secret);
+var Datastore = require('nedb');
+
+var clients = [];
+var userTokens = new Datastore({filename: 'userTokens.db', autoload: true});
+
+userTokens.find({}, function(err, tokens) {
+	tokens.forEach(function(token) {
+		clients.push(new User(tokens.access_token, tokens.access_token_secret));
+	});
+});
+
+function registerUser(token, secret) {
+	var auth = {
+		access_token: token,
+		access_token_secret: secret
+	};
+
+	userTokens.update({access_token: token}, auth, {upsert: true});
+}
+
+var express = require('express'),
+    OAuth = require('oauth').OAuth;
+
+var app = express();
+app.use(require('body-praser')());
+app.use(require('method-override')());
+app.use(require('cookie-parser')(ConsumerKey.consumer_key));
+app.use(require('express-session')());
+app.use(app.router);
+app.use(express.static(__dirname + '/public');
+
+//aouth
+var oa = new OAuth(
+  "https://api.twitter.com/oauth/request_token",
+  "https://api.twitter.com/oauth/access_token",
+  ConsumerKey.consumer_key,
+  ConsumerKey.consumer_secret,
+  "1.0",
+  "http://nandemosuru.ysfh.sexy/callback",
+  "HMAC-SHA1"
+);
+
+//auth/twitterにアクセスするとTwitterアプリケーション認証画面に遷移します。
+app.get('/auth', function(req, res){
+  oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+    if (error) {
+      console.log(error);
+      res.end("yeah no. didn't work.");
+    } else {
+      req.session.oauth = {};
+      req.session.oauth.token = oauth_token;
+      req.session.oauth.token_secret = oauth_token_secret;
+      res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token);
+    }
+  });
+});
+
+app.get('/callback', function(req, res, next){
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    var oauth = req.session.oauth;
+    oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
+    function(error, oauth_access_token, oauth_access_token_secret, results){
+      if (error){
+        console.log(error);
+        res.send("yeah something broke.");
+      } else {
+        req.session.oauth.access_token = oauth_access_token;
+        req.session.oauth.access_token_secret = oauth_access_token_secret;
+
+		registerUser(oauth_access_token, oauth_access_token_secret);
+
+		res.end("worked. nice one.");
+      }
+    });
+  } else {
+    next(new Error("you're not supposed to be here."));
+  }
+});
