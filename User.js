aa6b4a36968f2a35bc3a1cc88cc0fd7995e91990
猪野 湧斗 <ice.softicon30th@gmail.com>
2016-03-13 17:39:59 +0900

    self = this

--- User.js
+++ User.js
@@ -3,6 +3,7 @@ var EventEmitter = require('events').EventEmitter;
 
 function User(consumerKey, consumerSecret, accessToken, accessTokenSecret, screenName, userId) {
     EventEmitter.call(this);
+    var self = this;
     
 	var client = new Twit({
 		consumer_key: consumerKey,
@@ -14,15 +15,15 @@ function User(consumerKey, consumerSecret, accessToken, accessTokenSecret, scree
 	var stream = client.stream('user');
     
 	stream.on('tweet', function (tweet) {
-        this.emit('tweet', tweet);
+        self.emit('tweet', tweet);
     });
     
     stream.on('connected', function () {
-        this.emit('connected', screenName, userId);
+        self.emit('connected', screenName, userId);
     });
     
     stream.on('disconnect', function (message) {
-        this.emit('disconnect', message, screenName, userId);
+        self.emit('disconnect', message, screenName, userId);
     });
 }
 
